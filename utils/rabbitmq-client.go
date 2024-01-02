6e2104728a73507c1d5ee392fab220e9bbc80fb8
davisbento <contato@davisbento.dev>
2024-01-02 20:06:34 -0300

    remove amqp url

--- utils/rabbitmq-client.go
+++ utils/rabbitmq-client.go
@@ -1,7 +1,7 @@
 package utils
 
 const (
-	RabbitMQURL  = "amqps://fhfzbtsp:pp5oJ9VrHHcw_QgAoKoKTrPxFbHj9s39@jackal.rmq.cloudamqp.com/fhfzbtsp"
+	RabbitMQURL  = "amqp://"
 	QueueName    = "my_queue"
 	ExchangeName = "my_exchange"
 	RoutingKey   = "my_routing_key"
